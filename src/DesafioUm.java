024f71145c9335b324ce57f620a3437c3692fdbc
GilsonCoelho <coelho.gilson@gmail.com>
2023-07-14 23:11:50 -0300

    Desafio_Um

---
+++ src/DesafioUm.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class DesafioUm {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = 0;
+        List<Integer> pares = new ArrayList<>();
+        List<Integer> impares = new ArrayList<>();
+
+        n = scan.nextInt();
+
+        for (int i =0 ; i < n ; i++){
+            int entrada = scan.nextInt();
+            if (entrada % 2 == 0){
+                pares.add(entrada);
+            }else{
+                impares.add(entrada);
+            }
+        }
+        Collections.sort(pares);
+        Collections.sort(impares);
+        Collections.reverse(impares);
+
+        for(int par: pares) System.out.println(par);
+        for(int impar: impares) System.out.println(impar);
+
+
+
+
+        scan.close();
+    }
+}
